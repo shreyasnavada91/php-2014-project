bd39923fa7882c7a2998cf91a14d46b6e2fd73cb
Developer Name <user@example.com>
2013-02-04 00:00:00 +0530

    refactor: migrated from MySQLi to PDO

--- config/database.php
+++ config/database.php
@@ -1,14 +1,18 @@
 <?php
-$db_config = array('host' => 'localhost', 'username' => 'root', 'password' => 'password', 'database' => 'company_db');
+define('DB_HOST', 'localhost'); define('DB_NAME', 'company_db'); define('DB_USER', 'root'); define('DB_PASS', 'password'); define('DB_CHARSET', 'utf8');
 class Database {
-    private $mysqli; private static $instance = null;
+    private $pdo; private static $instance = null;
     private function __construct() {
-        $this->mysqli = new mysqli($GLOBALS['db_config']['host'], $GLOBALS['db_config']['username'], $GLOBALS['db_config']['password'], $GLOBALS['db_config']['database']);
-        if ($this->mysqli->connect_error) { die('Connection failed: ' . $this->mysqli->connect_error); }
-        $this->mysqli->set_charset($GLOBALS['db_config']['charset']);
+        try {
+            $this->pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASS, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC));
+        } catch (PDOException $e) { die('Connection failed: ' . $e->getMessage()); }
     }
     public static function getInstance() { if (self::$instance === null) { self::$instance = new Database(); } return self::$instance; }
-    public function query($sql) { return $this->mysqli->query($sql); }
-    public function prepare($sql) { return $this->mysqli->prepare($sql); }
+    public function getConnection() { return $this->pdo; }
+    public function query($sql, $params = array()) { $stmt = $this->pdo->prepare($sql); $stmt->execute($params); return $stmt; }
+    public function select($table, $where = '', $params = array()) { $sql = "SELECT * FROM " . $table; if ($where) { $sql .= " WHERE " . $where; } $stmt = $this->pdo->prepare($sql); $stmt->execute($params); return $stmt->fetchAll(); }
+    public function insert($table, $data) { $columns = implode(', ', array_keys($data)); $placeholders = ':' . implode(', :', array_keys($data)); $sql = "INSERT INTO " . $table . " (" . $columns . ") VALUES (" . $placeholders . ")"; $stmt = $this->pdo->prepare($sql); return $stmt->execute($data); }
+    public function update($table, $data, $where, $whereParams = array()) { $sets = array(); foreach (array_keys($data) as $column) { $sets[] = "$column = :$column"; } $sql = "UPDATE " . $table . " SET " . implode(', ', $sets) . " WHERE " . $where; $stmt = $this->pdo->prepare($sql); return $stmt->execute(array_merge($data, $whereParams)); }
+    public function delete($table, $where, $params = array()) { $sql = "DELETE FROM " . $table . " WHERE " . $where; $stmt = $this->pdo->prepare($sql); return $stmt->execute($params); }
 }
 ?>
--- models/User.php
+++ models/User.php
@@ -1,17 +1,16 @@
 <?php
 require_once 'config/database.php';
 class User {
-    private $id, $name, $email;
-    public function __construct($id = null, $name = null, $email = null) { $this->id = $id; $this->name = $name; $this->email = $email; }
+    private $id, $name, $email, $created_at;
+    public function __construct($id = null, $name = null, $email = null) { $this->id = $id; $this->name = $name; $this->email = $email; $this->created_at = date('Y-m-d H:i:s'); }
     public function save() {
-        $db = Database::getInstance(); $stmt = $db->prepare("INSERT INTO users (name, email) VALUES (?, ?)");
-        $stmt->bind_param("ss", $this->name, $this->email); return $stmt->execute();
+        $db = Database::getInstance();
+        if ($this->id) { return $db->update('users', array('name' => $this->name, 'email' => $this->email), 'id = :id', array(':id' => $this->id)); }
+        else { $data = array('name' => $this->name, 'email' => $this->email, 'created_at' => $this->created_at); return $db->insert('users', $data); }
     }
-    public static function getAll() {
-        $db = Database::getInstance(); $result = $db->query("SELECT * FROM users");
-        $users = array(); while ($row = $result->fetch_assoc()) { $users[] = $row; }
-        return $users;
-    }
+    public static function getAll() { $db = Database::getInstance(); return $db->select('users'); }
+    public static function getById($id) { $db = Database::getInstance(); return $db->query("SELECT * FROM users WHERE id = ?", array($id))->fetch(); }
+    public static function delete($id) { $db = Database::getInstance(); return $db->delete('users', 'id = ?', array($id)); }
     public function getId() { return $this->id; } public function getName() { return $this->name; } public function getEmail() { return $this->email; }
     public function setId($id) { $this->id = $id; } public function setName($name) { $this->name = $name; } public function setEmail($email) { $this->email = $email; }
 }
