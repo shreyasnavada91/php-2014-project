cdfd3ddcf6eb5897cb0a937227d40a45bb139dff
Developer Name <user@example.com>
2013-02-19 00:00:00 +0530

    feat: modernized with PHP 5.6, namespaces and traits

--- config/database.php
+++ config/database.php
@@ -1,18 +1,23 @@
 <?php
-define('DB_HOST', 'localhost'); define('DB_NAME', 'company_db'); define('DB_USER', 'root'); define('DB_PASS', 'password'); define('DB_CHARSET', 'utf8');
+namespace App\Database;
+use PDO; use PDOException;
 class Database {
-    private $pdo; private static $instance = null;
+    private static ?PDO $instance = null; private static ?self $singleton = null;
+    private PDO $connection; private string $host, $dbname, $username, $password, $charset;
     private function __construct() {
+        $this->host = 'localhost'; $this->dbname = 'company_db'; $this->username = 'root'; $this->password = 'password'; $this->charset = 'utf8';
         try {
-            $this->pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASS, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC));
-        } catch (PDOException $e) { die('Connection failed: ' . $e->getMessage()); }
+            $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset={$this->charset}";
+            $this->connection = new PDO($dsn, $this->username, $this->password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_PERSISTENT => true));
+        } catch (PDOException $e) { error_log("Database connection failed: " . $e->getMessage()); throw new RuntimeException("Database connection failed"); }
     }
-    public static function getInstance() { if (self::$instance === null) { self::$instance = new Database(); } return self::$instance; }
-    public function getConnection() { return $this->pdo; }
-    public function query($sql, $params = array()) { $stmt = $this->pdo->prepare($sql); $stmt->execute($params); return $stmt; }
-    public function select($table, $where = '', $params = array()) { $sql = "SELECT * FROM " . $table; if ($where) { $sql .= " WHERE " . $where; } $stmt = $this->pdo->prepare($sql); $stmt->execute($params); return $stmt->fetchAll(); }
-    public function insert($table, $data) { $columns = implode(', ', array_keys($data)); $placeholders = ':' . implode(', :', array_keys($data)); $sql = "INSERT INTO " . $table . " (" . $columns . ") VALUES (" . $placeholders . ")"; $stmt = $this->pdo->prepare($sql); return $stmt->execute($data); }
-    public function update($table, $data, $where, $whereParams = array()) { $sets = array(); foreach (array_keys($data) as $column) { $sets[] = "$column = :$column"; } $sql = "UPDATE " . $table . " SET " . implode(', ', $sets) . " WHERE " . $where; $stmt = $this->pdo->prepare($sql); return $stmt->execute(array_merge($data, $whereParams)); }
-    public function delete($table, $where, $params = array()) { $sql = "DELETE FROM " . $table . " WHERE " . $where; $stmt = $this->pdo->prepare($sql); return $stmt->execute($params); }
+    public static function getInstance(): self { if (self::$singleton === null) { self::$singleton = new self(); } return self::$singleton; }
+    public function getConnection(): PDO { return $this->connection; }
+    public function query(string $sql, array $parameters = []): PDOStatement { $stmt = $this->connection->prepare($sql); $stmt->execute($parameters); return $stmt; }
+    public function select(string $table, array $columns = ['*'], string $where = '', array $params = []): array { $columnsStr = implode(', ', $columns); $sql = "SELECT {$columnsStr} FROM {$table}"; if ($where) { $sql .= " WHERE {$where"; } $stmt = $this->query($sql, $params); return $stmt->fetchAll(); }
+    public function insert(string $table, array $data): int { $columns = implode(', ', array_keys($data)); $placeholders = ':' . implode(', :', array_keys($data)); $sql = "INSERT INTO {$table} ({$columns}) VALUES ({$placeholders})"; $stmt = $this->query($sql, $data); return (int)$this->connection->lastInsertId(); }
+    public function update(string $table, array $data, string $where, array $whereParams = []): int { $sets = []; foreach (array_keys($data) as $column) { $sets[] = "{$column} = :{$column}"; } $sql = "UPDATE {$table} SET " . implode(', ', $sets) . " WHERE {$where}"; $stmt = $this->query($sql, array_merge($data, $whereParams)); return $stmt->rowCount(); }
+    public function delete(string $table, string $where, array $params = []): int { $sql = "DELETE FROM {$table} WHERE {$where}"; $stmt = $this->query($sql, $params); return $stmt->rowCount(); }
 }
+trait ValidatesInput { protected array $errors = []; protected function validateRequired(string $field, string $value): bool { if (empty(trim($value))) { $this->errors[$field] = ucfirst($field) . ' is required'; return false; } return true; } public function getErrors(): array { return $this->errors; } public function hasErrors(): bool { return !empty($this->errors); } }
 ?>
