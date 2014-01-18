6f9076f7cee054b58e11fe06db4a6b24b32e784a
shreyasnavada <shreyasnavada.91@gmail.com>
2014-01-18 10:45:00 +0530

    fix: fixed PHP syntax in Product model

--- models/Product.php
+++ models/Product.php
@@ -1,10 +1,10 @@
 <?php
 class Product {
-    private \$db;
-    public function __construct() { \$this->db = DatabaseConfig::getInstance()->getConnection(); }
+    private $db;
+    public function __construct() { $this->db = DatabaseConfig::getInstance()->getConnection(); }
     public function getAll() {
-        \$result = \$this->db->query("SELECT * FROM products");
-        return \$result->fetch_all(MYSQLI_ASSOC);
+        $result = $this->db->query("SELECT * FROM products");
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 }
 ?>
